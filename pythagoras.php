c2f27dcae39620d8e1f7f67bf8b17700dbc698a9
roulichova1 <>
2018-10-01 13:01:41 +0000

    obdélník

---
+++ pythagoras.php
@@ -0,0 +1,14 @@
+<?php
+$a = 4;
+$b = 5;
+$obdelnikobsah = $a*$b;
+echo 'Obsah obdelníku o stranách '. $a.'cm a '. $b .'cm je: '. $obdelnikobsah. 'cm2.';
+
+//$strana = 6;
+//$uhel = 60;
+
+
+?>
+
+
+
